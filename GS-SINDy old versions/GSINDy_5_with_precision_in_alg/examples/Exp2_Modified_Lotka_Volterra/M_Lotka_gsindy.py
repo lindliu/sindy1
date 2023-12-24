d9b27875a4bd3eaffd9918c907ddf99e594b25e5
lindliu <dliu2020@outlook.com>
2023-12-24 23:10:20 +0100

    f

---
+++ GS-SINDy old versions/GSINDy_5_with_precision_in_alg/examples/Exp2_Modified_Lotka_Volterra/M_Lotka_gsindy.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 22:41:15 2023
+
+@author: dliu
+"""
+
+
+import sys
+sys.path.insert(1, '../../GSINDy')
+sys.path.insert(1, '../..')
+sys.path.insert(1, '../tools')
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from M_Lotka_constants import get_basis_functions
+import M_Lotka_constants as constants
+np.set_printoptions(formatter={'float': lambda x: "{0:.4f}".format(x)})
+
+
+########## hyper parameters ###########
+ensemble = constants.ensemble
+precision = constants.precision
+deriv_spline = constants.deriv_spline
+alpha = constants.alpha
+threshold_sindy_list = constants.threshold_sindy_list
+threshold_group_list = constants.threshold_group_list
+threshold_similarity_list = constants.threshold_similarity_list
+
+########## function variable ###########
+t = constants.t
+x0_list = constants.x0_list
+a_list = constants.a_list
+
+func = constants.func
+real_list = constants.real_list
+
+########## basis functions and optimizer ###########
+basis_type = constants.basis_type
+basis, opt = get_basis_functions(basis_type=basis_type, GSINDY=True)
+basis_functions_list = basis['functions']
+basis_functions_name_list = basis['names']
+
+
+path_base = os.path.join(os.getcwd(), 'results')
+suffix = f'{basis_type}_SQTL' if opt=='Manually' else f'{basis_type}_{opt}'
+if __name__ == "__main__":
+    
+    from gsindy_2d_train import gsindy_2d_train
+    gsindy_2d_train(func, t, x0_list, a_list, real_list, suffix, basis, precision, alpha, opt, deriv_spline, ensemble, path_base,
+                    threshold_sindy_list, threshold_group_list, threshold_similarity_list)
